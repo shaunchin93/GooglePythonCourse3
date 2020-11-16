62b8f9cc117f5b5341b99a93a7e2175d84aa236a
jhctkjh <74557058+jhctkjh@users.noreply.github.com>
2020-11-16 12:04:36 -0500

    Update healthcheck.py
    Added comments to explain the purpose of each function. Added spaces for better readability

--- healthcheck.py
+++ healthcheck.py
@@ -1,12 +1,18 @@
 import shutil
 import psutil
+
 def check_disk_usage(disk):
+    #This function will check disk usage
     du=shutil.disk_usage(disk)
     free=du.free/du.total*100
     return free>20
+
 def check_cpu_usage():
+    #This function will check cpu usage
     usage=psutil.cpu_percent(0.5)
     return usage>75
+
+#Output messages after completed checks
 if not check_disk_usage("/"):
     print("Error in Disk!")
 elif not check_cpu_usage():
